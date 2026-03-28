a7a94daead5e9f952432ae6bf7de036d785963f4
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2026-03-29 02:47:29 +0500

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
 
 CONFIG = {
-    "gmail_user": "your_email@gmail.com",
-    "gmail_pass": "your_password",
-    "whatsapp_token": "your_whatsapp_api_token"
+    "gmail_user": os.getenv("GMAIL_USER"),
+    "gmail_pass": os.getenv("GMAIL_PASS"),
+    "whatsapp_token": os.getenv("WHATSAPP_TOKEN")
 }
