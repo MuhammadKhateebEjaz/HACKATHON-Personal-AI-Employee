f7e5f1bb7480ba355d925ae0fcf315e1ad396fa6
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2026-03-29 02:48:14 +0500

    Update whatsapp_watcher.py

--- whatsapp_watcher.py
+++ whatsapp_watcher.py
@@ -1,8 +1,7 @@
-
 class WhatsAppWatcher:
     def __init__(self, token):
         self.token = token
 
     def start(self):
         print("[WHATSAPP] Watching messages...")
-        # Placeholder
+        # Placeholder for WhatsApp API logic
